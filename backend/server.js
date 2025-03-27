a53550be5f885d46831614211c65ad06528c23ac
lukapejic <e12221708@student.tuwien.ac.at>
2025-03-27 14:59:57 +0100

    Fixed dependencies so the frontend work can start

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import productRoutes from "./routes/product.route.js"
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json()); //allows us to accept JSON data in the body
 
